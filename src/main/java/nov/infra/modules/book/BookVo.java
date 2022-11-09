b7d677b34ad6be19f2c8884f8f81606c6e92bb40
hyeon <gwg01051@naver.com>
2022-11-09 16:44:36 +0900

    likebook list수정

--- src/main/java/nov/infra/modules/book/BookVo.java
+++ src/main/java/nov/infra/modules/book/BookVo.java
@@ -4,6 +4,7 @@ public class BookVo {
 	
 	private String name;
 	private String seq;
+	private String seq1;
 	
 	public String getName() {
 		return name;
@@ -17,4 +18,11 @@ public class BookVo {
 	public void setSeq(String seq) {
 		this.seq = seq;
 	}
+	public String getSeq1() {
+		return seq1;
+	}
+	public void setSeq1(String seq1) {
+		this.seq1 = seq1;
+	}
+	
 }
