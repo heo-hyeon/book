3737ab422ffd41f2bae836f4a89eed4ba2847ab6
hyeon <gwg01051@naver.com>
2022-12-07 18:35:03 +0900

    service수정

--- src/main/java/nov/infra/modules/book/BookService.java
+++ src/main/java/nov/infra/modules/book/BookService.java
@@ -6,13 +6,14 @@ public interface BookService {
 	
 	public Book selectOne(BookVo vo) throws Exception;
 	
-	public Book selecOneFv(BookVo vo) throws Exception;
+	public List<Book> selectList(BookVo vo) throws Exception;
 	
 	public List<Book> selectlistTag(BookVo vo) throws Exception;
 	
 	public List<Book> selectlistReview(BookVo vo) throws Exception;
 	
-	public List<Book> selectList(BookVo vo) throws Exception;
+	
+	public Book selecOneFv(BookVo vo) throws Exception;
 	
 	public List<Book> selectListFv(BookVo vo) throws Exception;
 	
