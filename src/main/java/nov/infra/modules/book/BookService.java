d128e0ae38e67f4b573a23af477f8d291e305c18
hyeon <gwg01051@naver.com>
2022-12-10 16:40:38 +0900

    service수정

--- src/main/java/nov/infra/modules/book/BookService.java
+++ src/main/java/nov/infra/modules/book/BookService.java
@@ -12,11 +12,10 @@ public interface BookService {
 	
 	public List<Book> selectlistReview(BookVo vo) throws Exception;
 	
-	//좋아요 누른 책
-	public Book selecOneFv(BookVo vo) throws Exception;
-	
-	public List<Book> selectListFv(BookVo vo) throws Exception;
-	
-	public List<Book> selectlistTagFv(BookVo vo) throws Exception;
+//	public Book selecOneFv(BookVo vo) throws Exception;
+//	
+//	public List<Book> selectListFv(BookVo vo) throws Exception;
+//	
+//	public List<Book> selectlistTagFv(BookVo vo) throws Exception;
 }
 
